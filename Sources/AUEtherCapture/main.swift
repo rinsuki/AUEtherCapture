57c3cc50a4907b80841174cfa887f252b2e8e392
rinsuki <428rinsuki+git@gmail.com>
2021-03-01 11:18:26 +0900

    Skip Loopback device

--- Sources/AUEtherCapture/main.swift
+++ Sources/AUEtherCapture/main.swift
@@ -2,7 +2,7 @@ import Foundation
 import Pcap
 //print(Pcap.Device.all())
 
-let session = try Pcap.CaptureSession(device: Pcap.Device.all().first!)
+let session = try Pcap.CaptureSession(device: Pcap.Device.all().filter { $0.name != "\\Device\\NPF_Loopback" && $0.name != "lo0" }.first!)
 
 print("session started", session)
 
