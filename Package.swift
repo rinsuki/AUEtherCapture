4273783c7ff589870d263e7305bf2bd84f7cad0b
rinsuki <428rinsuki+git@gmail.com>
2021-03-04 15:04:27 +0900

    Use patched version of StarScream
    https://github.com/daltoniam/Starscream/pull/844

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .package(url: "https://github.com/rinsuki/SwiftMsgPack", .branch("implement/handling-nsnull")),
         .package(name: "Gzip", url: "https://github.com/rinsuki/GzipSwift", .branch("support-windows")),
         .package(name: "SocketIO", url: "https://github.com/socketio/socket.io-client-swift", from: "15.2.0"),
+        .package(url: "https://github.com/tesseract-one/Starscream", .revision("44e25dd0af700a4da61d293eca8a4e1b90da97fc")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
